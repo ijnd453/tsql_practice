716c86af54c4bf7570ec6fb46cf30354b94c636c
ijnd <Isaiah Davis>
2020-03-02 21:50:39 -0500

    lab2 load statements completed

---
+++ completed/lab2-load1.sql
@@ -0,0 +1,10 @@
+USE SalesLT;
+CREATE TABLE Address (
+	City VARCHAR(15),
+	StateProvince VARCHAR(15),
+	ZIP VARCHAR(10)
+);
+INSERT INTO Address VALUES 
+('Ann Arbor', 'Michigan', '18402'),
+('Chicago', 'Illinois', '50143'),
+('Paris', 'France', '11525-241');
